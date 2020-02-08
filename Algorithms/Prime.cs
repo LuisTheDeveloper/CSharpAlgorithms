aea4eaeac82f3ce2d4fd08d1c5be5dc7790a141d
LuisTheDeveloper <Luis.Pedro.Fonseca@outlook.com>
2020-02-08 22:58:49 +0000

    Add project files.

---
+++ Algorithms/Prime.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class Prime
+    {
+        public void CalculatePrime(int minNum, int maxNum)
+        {
+            var PrimeList = new List<int>();
+            var NonPrime = new List<int>();
+
+            for (int i = minNum; i <= maxNum; i++)
+            {
+                for(int j = 2; j <= i; j++)
+                {
+                    var res = i % j;
+                    if (i != j)
+                    {
+                        if (res != 0)
+                        {
+                            if (!NonPrime.Contains(i) && !PrimeList.Contains(i))
+                            {
+                                PrimeList.Add(i);
+                            }
+                        }
+                        else
+                        {
+                            if (PrimeList.Contains(i))
+                                PrimeList.Remove(i);
+                            NonPrime.Add(i);
+                        }
+                    }
+                    else
+                    {
+                        if (res == 0 && !NonPrime.Contains(i) && !PrimeList.Contains(i))
+                        {
+                            PrimeList.Add(i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach(int num in PrimeList)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+
+        public void ElementsList()
+        {
+            int[] numList = { 2, 38, 15, 38, 5, 14, 21, 99, 21 };
+
+            List<int> list2 = new List<int>();
+            List<int> list3 = new List<int>();
+            List<int> list4 = new List<int>();
+            List<int> list5 = new List<int>(numList);
+
+            bool FoundTwice = false;
+
+            foreach (int num in numList)
+            {
+                if (!list2.Contains(num))
+                {
+                    list2.Add(num);
+                }
+                else
+                {
+                    list3.Add(num);
+                }
+            }
+
+            Console.WriteLine("First Method:");
+            list3.Sort();
+            for (int i = 0; i < list3.Count(); i++)
+            {
+                Console.WriteLine(list3[i]);
+            }
+
+            Console.WriteLine("Second Method:");
+            foreach (int num in numList)
+            {
+                FoundTwice = false;
+                foreach (int element in numList)
+                {
+                    if (element == num && FoundTwice == false)
+                        FoundTwice = true;
+                    else if (element == num && FoundTwice == true)
+                    {
+                        if (!list4.Contains(element))
+                        {
+                            list4.Add(element);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            for (int i = 0; i < list4.Count(); i++)
+            {
+                Console.WriteLine(list4[i]);
+            }
+            list5.Sort();
+            Console.WriteLine("Third Method:");
+            List<int> distinctList = list5.Distinct().ToList();
+            foreach (int value in distinctList)
+            {
+                Console.WriteLine($"Value: {value}");
+            }
+        }
+         
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Prime numprime = new Prime();
+            /*
+            Console.WriteLine("Type Minimum: ");
+            var MinN = Console.ReadLine();
+            int MinNum = Int32.Parse(MinN);
+            Console.WriteLine("Type Maximum: ");
+            var MaxN = Console.ReadLine();
+            int MaxNum = Int32.Parse(MaxN);
+            numprime.CalculatePrime(MinNum,MaxNum);*/
+            numprime.ElementsList();
+ 
+        }
+    }
+}
